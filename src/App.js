da34874290e7bd95020f66c8ee5afc2ac162f73c
Ankita Sood <ankisood@amazon.com>
2024-09-03 22:07:35 +0530

    fixed css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
@@ -38,7 +15,6 @@ function App() {
       <Tours />
       <Footer />
     </>
->>>>>>> 7f45205 (BackRoad app)
   );
 }
 
